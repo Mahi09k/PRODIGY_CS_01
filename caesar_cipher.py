f81939e58d0bcf4c68f57b46ecec8eee0da24d54
Mahesh Kumar Kolla <mahi444k0@gmail.com>
2024-05-15 17:23:30 +0530

    Decryption Added

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,24 +1,68 @@
 # Encryption Algorithm
 def encrypt(text, s):
     result = ""
-
-    # traverse text
+    # Traverse text
     for i in range(len(text)):
         char = text[i]
-
         # Encrypt uppercase characters
         if char.isupper():
             result += chr((ord(char) + s - 65) % 26 + 65)
-
         # Encrypt lowercase characters
-        else:
+        elif char.islower():
             result += chr((ord(char) + s - 97) % 26 + 97)
-
+        # If it's not a letter, leave it as is
+        else:
+            result += char
     return result
 
+# Decryption Algorithm
+def decrypt(text, s):
+    result = ""
+    # Traverse text
+    for i in range(len(text)):
+        char = text[i]
+        # Decrypt uppercase characters
+        if char.isupper():
+            result += chr((ord(char) - s - 65) % 26 + 65)
+        # Decrypt lowercase characters
+        elif char.islower():
+            result += chr((ord(char) - s - 97) % 26 + 97)
+        # If it's not a letter, leave it as is
+        else:
+            result += char
+    return result
 
-text = input("Enter your Message to Encrypt: ")
-s = int(input("Enter the shift value: "))
-print("Text : " + text)
-print("Shift Value : " + str(s))
-print("The Encrypted text is : " + encrypt(text, s))
+# Main function with menu
+def main():
+    while True:
+        print("Choose an option:")
+        print("1. Encrypt a message")
+        print("2. Decrypt a message")
+        print("3. Exit")
+        choice = input("Enter your choice (1/2/3): ")
+
+        if choice == '1':
+            text = input("Enter your Message to Encrypt: ")
+            s = int(input("Enter the shift value: "))
+            encrypted_text = encrypt(text, s)
+            print("Text : " + text)
+            print("Shift Value : " + str(s))
+            print("The Encrypted text is : " + encrypted_text)
+
+        elif choice == '2':
+            text = input("Enter your Message to Decrypt: ")
+            s = int(input("Enter the shift value: "))
+            decrypted_text = decrypt(text, s)
+            print("Text : " + text)
+            print("Shift Value : " + str(s))
+            print("The Decrypted text is : " + decrypted_text)
+
+        elif choice == '3':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid choice. Please choose 1, 2, or 3.")
+
+if __name__ == "__main__":
+    main()
