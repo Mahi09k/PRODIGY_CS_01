441cb4b11ed00f50ca12006184a8e31a8ddedce9
Mahesh Kumar Kolla <mahi444k0@gmail.com>
2024-05-15 17:41:59 +0530

    done some changes

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,41 +1,9 @@
-# Encryption Algorithm
-def encrypt(text, s):
-    result = ""
-    # Traverse text
-    for i in range(len(text)):
-        char = text[i]
-        # Encrypt uppercase characters
-        if char.isupper():
-            result += chr((ord(char) + s - 65) % 26 + 65)
-        # Encrypt lowercase characters
-        elif char.islower():
-            result += chr((ord(char) + s - 97) % 26 + 97)
-        # If it's not a letter, leave it as is
-        else:
-            result += char
-    return result
+import pyfiglet
 
-# Decryption Algorithm
-def decrypt(text, s):
-    result = ""
-    # Traverse text
-    for i in range(len(text)):
-        char = text[i]
-        # Decrypt uppercase characters
-        if char.isupper():
-            result += chr((ord(char) - s - 65) % 26 + 65)
-        # Decrypt lowercase characters
-        elif char.islower():
-            result += chr((ord(char) - s - 97) % 26 + 97)
-        # If it's not a letter, leave it as is
-        else:
-            result += char
-    return result
-
-# Main function with menu
+# Create a menu with big text
 def main():
     while True:
-        print("Choose an option:")
+        print(pyfiglet.figlet_format("CAESAR CIPHER"))
         print("1. Encrypt a message")
         print("2. Decrypt a message")
         print("3. Exit")
@@ -47,7 +15,7 @@ def main():
             encrypted_text = encrypt(text, s)
             print("Text : " + text)
             print("Shift Value : " + str(s))
-            print("The Encrypted text is : " + encrypted_text)
+            print("Encrypted Text : " + str(encrypted_text))
 
         elif choice == '2':
             text = input("Enter your Message to Decrypt: ")
@@ -55,14 +23,41 @@ def main():
             decrypted_text = decrypt(text, s)
             print("Text : " + text)
             print("Shift Value : " + str(s))
-            print("The Decrypted text is : " + decrypted_text)
+            print("Decrypted Text : " + str(decrypted_text))
+
 
         elif choice == '3':
-            print("Exiting the program.")
+            print(pyfiglet.figlet_format("Goodbye!"))
             break
 
         else:
             print("Invalid choice. Please choose 1, 2, or 3.")
+
+# Encryption Algorithm
+def encrypt(text, s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + s - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + s - 97) % 26 + 97)
+        else:
+            result += char
+    return result
+
+# Decryption Algorithm
+def decrypt(text, s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) - s - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) - s - 97) % 26 + 97)
+        else:
+            result += char
+    return result
 
 if __name__ == "__main__":
     main()
